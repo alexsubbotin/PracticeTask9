5b6070d35367ab1147b0a9a54e85f5e219d6203d
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 11:45:27 +0500

    Demo of the list creation added

--- PracticeTask9/Program.cs
+++ PracticeTask9/Program.cs
@@ -8,8 +8,21 @@ namespace PracticeTask9
 {
     class Program
     {
+        // Task: create a circular list of N elements with values from 1 to N (1 is the tail).
+        // Add functions to search and delete elements.
+        // Student: Alexey Subbotin. Group: SE-17-1.
         static void Main(string[] args)
         {
+            // Getting the N.
+            Console.Write("Enter the N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            // Creating the list.
+            CircularList circularList = new CircularList();
+            circularList.CreateCircularList(N);
+            circularList.Show();
+
+            Console.ReadLine();
         }
     }
 }
